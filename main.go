065b04b4efbc0a285353567da39a53fec7271baf
Joe Granville <874605+jwgranville@users.noreply.github.com>
2026-07-20 20:34:36 -0700

    feat(auth): persist tokens and add auth status and logout

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,14 +14,18 @@ import (
 )
 
 const contentType = "application/json"
-
 const devBaseURL = "http://localhost:8000"
+const tokenPath = ".go-joe-token"
 
+const authRoute = "/auth"
 const loginRoute = "/login"
+const logoutRoute = "/logout"
 const pingRoute = "/ping"
 const pongRoute = "/pong"
 
+const devAuthURL = devBaseURL + authRoute
 const devLoginURL = devBaseURL + loginRoute
+const devLogoutURL = devBaseURL + logoutRoute
 
 const devPingURL = devBaseURL + pingRoute
 const devPingsURL = devBaseURL + "/pings"
@@ -34,8 +39,17 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
 type NewPingRequest struct {
 	Number uint `json:"number"`
+}
+
+type readCloser struct {
+	io.Reader
+	io.Closer
 }
 
 func main() {
@@ -47,8 +61,12 @@ func main() {
 
 	subcommand := os.Args[1]
 	switch subcommand {
+	case "auth":
+		runAuth()
 	case "login":
 		runLogin(os.Args[2:])
+	case "logout":
+		runLogout()
 	case "ping":
 		runPing(os.Args[2:])
 	case "pings":
@@ -59,6 +77,33 @@ func main() {
 		fmt.Printf("%q not recognized.\n", subcommand)
 		printUsage()
 	}
+}
+
+func runAuth() {
+	token, err := os.ReadFile(tokenPath) // Factor this into a helper
+	if err != nil {
+		fmt.Println("Could not read saved token:", err)
+		return
+	}
+
+	// This too
+	request, err := http.NewRequest(http.MethodGet, devAuthURL, nil)
+	if err != nil {
+		fmt.Println("Could not create auth request:", err)
+		return
+	}
+
+	request.Header.Set("Authorization", string(token))
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		fmt.Println("Auth request failed:", err)
+		return
+	}
+	err = printResponse(resp)
+    if err != nil {
+        return
+    }
 }
 
 func runLogin(args []string) {
@@ -84,7 +129,52 @@ func runLogin(args []string) {
 		return
 	}
 
-	httpPostTo(devLoginURL, contentType, bytes.NewReader(body))
+	responseBytes, status, err := httpPostTo(
+		devLoginURL,
+		contentType,
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		return
+	}
+
+	if status != http.StatusOK {
+		return
+	}
+
+	response := LoginResponse{}
+	err = json.Unmarshal(responseBytes, &response)
+	if err != nil {
+		fmt.Println("Could not decode login response:", err)
+		return
+	}
+
+	if response.Token == "" {
+		fmt.Println("Login response did not contain a token.")
+		return
+	}
+
+	saveTokenToFile([]byte(response.Token))
+}
+
+func runLogout() {
+	_, status, err := httpGetFrom(devLogoutURL)
+	if err != nil {
+		return
+	}
+
+	if status != http.StatusOK {
+		return
+	}
+
+	err = os.Remove(tokenPath)
+	if errors.Is(err, os.ErrNotExist) {
+	    return
+	}
+	if err != nil {
+		fmt.Println("Could not remove saved token:", err)
+		return
+	}
 }
 
 func runPing(args []string) {
@@ -151,29 +241,51 @@ func runPongs() {
 	httpGetFrom(devPongsURL)
 }
 
-func httpGetFrom(url string) {
+func httpGetFrom(url string) ([]byte, int, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Println("HTTP GET request failed:", err)
-		return
+		return nil, 0, err
 	}
 
-	printResponse(resp)
+	status := resp.StatusCode
+
+	var responseBody bytes.Buffer
+	bodyReader := io.TeeReader(resp.Body, &responseBody)
+	resp.Body = readCloser{Reader: bodyReader, Closer: resp.Body}
+
+	err = printResponse(resp)
+	if err != nil {
+		return nil, status, err
+	}
 
 	// Actually pretty print the response later
+
+	return responseBody.Bytes(), status, nil
 }
 
-func httpPostTo(url, mediaType string, body io.Reader) {
+func httpPostTo(url, mediaType string, body io.Reader) ([]byte, int, error) {
 	resp, err := http.Post(url, mediaType, body)
 	if err != nil {
 		fmt.Println("HTTP POST request failed:", err)
-		return
+		return nil, 0, err
 	}
 
-	printResponse(resp)
+	status := resp.StatusCode
+
+	var responseBody bytes.Buffer
+	bodyReader := io.TeeReader(resp.Body, &responseBody)
+	resp.Body = readCloser{Reader: bodyReader, Closer: resp.Body}
+
+	err = printResponse(resp)
+	if err != nil {
+		return nil, status, err
+	}
+
+	return responseBody.Bytes(), status, nil
 }
 
-func printResponse(resp *http.Response) {
+func printResponse(resp *http.Response) error {
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
@@ -183,10 +295,19 @@ func printResponse(resp *http.Response) {
 	_, err := io.Copy(os.Stdout, resp.Body)
 	if err != nil {
 		fmt.Println("Could not read response:", err)
-		return
+		return err
 	}
 
 	fmt.Println()
+	return nil
+}
+
+func saveTokenToFile(value []byte) {
+	err := os.WriteFile(tokenPath, value, 0600)
+	if err != nil {
+		fmt.Println("Could not write token config file:", err)
+		return
+	}
 }
 
 func printUsage() {
@@ -201,9 +322,9 @@ To do:
 - List pongs X
 - Implement login X
 - Move password input to ReadPassword X
-- Implement token caching (in a config file, possibly hidden?)
-- Implement logout
-- Implement authentication status report
+- Implement token caching (in a config file, possibly hidden?) X
+- Implement logout X
+- Implement authentication status report X
 - Implement create pong request
 - Implement change ping
 - Implement change pong
